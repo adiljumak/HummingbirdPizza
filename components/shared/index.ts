8dbf08de6fa8e8b634dad514d6acb090caee65f5
Adilzhan <a.dzhumakanov@arbuz.kz>
2025-01-30 13:55:52 +0500

    first commit

---
+++ components/shared/index.ts
@@ -0,0 +1,8 @@
+export { Container } from './container';
+export { Categories } from './categories';
+export { Header } from './header';
+export { Title } from './title'
+export { SortPopup } from './sort-popup';
+export { TopBar } from './top-bar';
+export { FilterCheckbox } from './filter-checkbox';
+export { Filters } from './filters'
